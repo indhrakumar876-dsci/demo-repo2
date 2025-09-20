c3f23eb7a7ab86b373c01d43c326a5e1eaf777c5
indhrakumar876-dsci <indhrakumar876@gmail.com>
2025-09-20 13:14:03 +0530

    this was a python demo

---
+++ demo.py
@@ -0,0 +1,18 @@
+def max_number(num):
+    num=max<=0
+    return num
+
+s1=int(input("enter first number"))
+s2=int(input("enter second number"))
+s3=int(input("enter third number"))
+if (s1>s2):
+    if(s1>s3):
+        print(s1,"is max")
+    else:
+        print(s3,"is max")
+else:
+    if(s2>s3):
+        print(s2,"is max")
+    else:
+        print(s3,"is max")
+     
